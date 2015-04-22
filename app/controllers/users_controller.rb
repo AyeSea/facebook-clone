7aa738f4c350530982368bafaaf88e7b7c05aae4
Andrew Chung <andrew.chung159@gmail.com>
2015-04-22 10:59:49 -0400

    Redirect user to their profile page on successful signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+	def show
+		if current_user == User.find(params[:id])
+			@user = current_user
+		else
+			@user = User.find(params[:id])
+		end
+	end
 end
